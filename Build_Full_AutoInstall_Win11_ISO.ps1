b731b8145d1a0bb20a48b2a9d870b11caf9a86b3
YantraByte <admin@yantrabyte.com>
2026-09-11 20:36:27 +0530

    fix(iso): resolve password and power settings crash by enabling seclogon, SensorService, VaultSvc and restoring safe power schemes

--- Build_Full_AutoInstall_Win11_ISO.ps1
+++ Build_Full_AutoInstall_Win11_ISO.ps1
@@ -206,8 +206,8 @@ reg add "HKU\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDes
 reg add "HKU\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{5399E694-625E-4428-8A00-569D01280786}" /t REG_DWORD /d 1 /f >nul 2>&1
 
 :: Performance & Menu Response Tweaks
-powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 >nul 2>&1
-for /f "tokens=4" %%a in ('powercfg -list ^| findstr /i "Ultimate Performance"') do powercfg -setactive %%a >nul 2>&1
+powercfg -restoredefaultschemes >nul 2>&1
+powercfg -setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c >nul 2>&1 || powercfg -setactive 381b4222-f694-41f0-9685-ff5bb260df2e >nul 2>&1
 
 reg add "HKU\.DEFAULT\Control Panel\Desktop" /v "MenuShowDelay" /t REG_SZ /d "20" /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d 0 /f >nul 2>&1
